926a10d34cd5fe1597afb401ee3195e7cd716450
eleanorstrib <hello@eleanorstrib.com>
2017-10-14 16:24:53 -0700

    added a test case, passed, renamed 'list' to 'list_' - no changes after viewing solution

--- maxsizelist.py
+++ maxsizelist.py
@@ -1,23 +1,28 @@
 class MaxSizeList(object):
     def __init__(self, value):
-        self.list = []
+        self.list_ = []
         self.max_size = value
 
     def push(self, word):
-        self.list.append(word)
-        if len(self.list) > self.max_size:
-            self.list.pop(0)
+        self.list_.append(word)
+        if len(self.list_) > self.max_size:
+            self.list_.pop(0)
 
     def get_list(self):
-        return self.list
+        return self.list_
 
 
 
 m = MaxSizeList(3)
-
 m.push("hey")
 m.push("hi")
 m.push("let\'s")
 m.push("go")
-
 print(m.get_list())
+
+n = MaxSizeList(1)
+n.push("hey")
+n.push("hi")
+n.push("let\'s")
+n.push("go")
+print(n.get_list())
