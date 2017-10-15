fcb7688960eefdacbba7ee7cb1a0eb3fbc0a851c
eleanorstrib <hello@eleanorstrib.com>
2017-10-15 16:20:59 -0700

    more work on assignment

--- subclassing.py
+++ subclassing.py
@@ -4,7 +4,8 @@ https://www.safaribooksonline.com/library/view/python-beyond-the/9781771373609/v
 
 Assignment:
 - Create a parent class and subclass it twice with LogFile and DelimFile classes
-- Write method in both - should be added to parent
+- Write method is in both child classes - should be added to parent
+-
 - Write method differences in child classes:
     - Logfile takes a string and writes to file with the date and time
       in the format YYYY-MM-DD HH:MM:SS
@@ -12,20 +13,34 @@ Assignment:
       for enabling user to put in values with a comma in them (e.g. ['4', '2,4', '7'])
 
 """
-
+import abc
 import csv
 from time import strftime as st
 
 
 class WriteFile(object):
-    def write(self, message, filename):
-        with open(filename, 'w', newline='') as csvfile:
+    __metaclass__ = abc.ABCMeta
+
+    def __init__(self, filename):
+        self.filename = ''
+
+    def set_filename(self, filename):
+        if not isinstance(filename, str):
+            self.filename = 'file.txt'
+        else:
+            self.filename = filename
+
+    def write(self, message):
+        with open(self.filename, 'w', newline='') as csvfile:
             new_row = csv.writer(csvfile, delimiter, ',')
 
 
 class LogFile(WriteFile, filename):
-    #new_row.writerow(st("%Y-%m-%d %H:%M:%S"), message)
-    pass
+    def set_filename(self, filename):
+
+    def write(self, message, filename):
+        new_row.writerow(st("%Y-%m-%d %H:%M:%S"), message)
+
 
 class DelimFile(WriteFile, filename, delimiter):
     pass
